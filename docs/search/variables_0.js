84211bac97a49b4a652f4c81b4f80976a0e6983e
Martin Juul <code@juul.xyz>
2021-08-05 11:07:01 +0200

    update docs

--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['cert_40',['cert',['../webserver_8h.html#af5f4558c0d2bb5827f079489adc4e000',1,'webserver.h']]],
-  ['clientaddress_41',['clientAddress',['../webserver_8cpp.html#af86c5d9a33a661ea6463bd4c6845122d',1,'webserver.cpp']]],
-  ['clientsocket_42',['clientSocket',['../webserver_8cpp.html#a116ad46e6edf05b4b27fac4960ea4939',1,'webserver.cpp']]]
+  ['client_5faddress_41',['client_address',['../webserver_8cpp.html#af660b380340c6f6f9b1b1a3626d09b53',1,'webserver.cpp']]],
+  ['client_5fsocket_42',['client_socket',['../webserver_8cpp.html#a98b17440ac4b68bfe5cf770e08a417cf',1,'webserver.cpp']]]
 ];
