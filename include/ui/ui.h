335c366c4d85f4b9d6d2fa8e7d3f652e1210b162
Martin Juul <code@juul.xyz>
2021-08-10 10:36:41 +0200

    docs(ui): expand Display::* methods

--- include/ui/ui.h
+++ include/ui/ui.h
@@ -9,17 +9,34 @@ class Display {
 public:
   /**
    * Initialize the display
+   *
+   * @retval void
    */
   void init();
   /**
    * Write a log message to the display
    *
    * @param text Text to write
+   *
+   * @retval void
    */
   void log(uint8_t *text);
 
+  /**
+    * Clears a line on the display
+    *
+    * @retval void
+    */
   void clear_line(int line_no);
 
+  /**
+    * Update stats display
+    *
+    * @param stats pointer to mbed_stats_cpu_t
+    * @param sample_time_ms Polling time in milliseconds. Used for calculating usage percentage.
+    *
+    * @retval void
+    */
   void update_stats(mbed_stats_cpu_t *stats, int sample_time_ms);
 };
 
